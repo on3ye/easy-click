c986a9e9b2d753095d0a508ebb6245808c449afa
on3ye <on3ye@proton.me>
2024-07-12 11:00:17 +0100

    Declare activation function

---
+++ include/activate.h
@@ -0,0 +1,7 @@
+// @author: on3ye
+// @date: 11-07-2024 13:45
+// @title: Activation Function Header File (activate.h)
+
+#include <gtk/gtk.h>
+
+void activate(GtkApplication* app, gpointer user_data);
