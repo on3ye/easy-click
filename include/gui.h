bee766c38b0e9a8f7b8b90c1425ee742d0e62e36
on3ye <on3ye@proton.me>
2024-07-15 05:13:11 +0100

    Add some credits and include autoclick.h

--- include/gui.h
+++ include/gui.h
@@ -1,4 +1,10 @@
+// @author: on3ye
+// @date: 13-07-2024 09:00
+// @title: GUI Render Header File (gui.h)
+
 #include <gtk/gtk.h>
 #include "signals.h"
+#include "autoclick.h"
 
+// Widgets Renderer Function declaration
 void render_widgets(GtkWindow* window);
