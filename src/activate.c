0cb0d66fa88afdc4e0ce72428942089e74bb59d4
on3ye <on3ye@proton.me>
2024-07-15 05:14:36 +0100

    Add some comment and include gui.h <Change printf to g_print>

--- src/activate.c
+++ src/activate.c
@@ -3,8 +3,8 @@
 // @title: Main Activation Callback Logic (activate.c)
 
 #include "activate.h"
-#include "gui.h"
 
+// Create the main window 
 void activate(GtkApplication* app, gpointer user_data) {
     // Window's Title: "Easy Click v(*Version*)"
     const char* TITLE = "Easy Click v0.1";
@@ -19,20 +19,21 @@ void activate(GtkApplication* app, gpointer user_data) {
     // Create new application window widget (Cast it to a GtkWindow* type)
     GtkWidget* w_Window = gtk_application_window_new(app);
     GtkWindow* window = GTK_WINDOW(w_Window);
-    printf("Initialized new application window!\n"); // [DEBUG INFO]
+    g_print("[+] Initialized new application window!\n"); // [DEBUG INFO]
 
     // Configure some window's properties
     gtk_window_set_title(window, TITLE);
     gtk_window_set_default_size(window, WIDTH, HEIGHT);
     gtk_window_set_resizable(window, RESIZABLE);
-    printf(
-        "Configured window:\n\t- Title: %s\n\t- Geometry: %dx%d\n\t- Resizable: %s\n",
+    g_print(
+        "[*] Configured window:\n\t- Title: %s\n\t- Geometry: %dx%d\n\t- Resizable: %s\n",
         gtk_window_get_title(window), WIDTH, HEIGHT, (gtk_window_get_resizable(window) == FALSE) ? "FALSE" : "TRUE"
     ); // [DEBUG INFO]
 
     gtk_window_present(window);
-    printf("GtkWindow created successfully!\n"); // [DEBUG INFO]
+    g_print("[+] GtkWindow created successfully!\n"); // [DEBUG INFO]
 
-    // Render all widgets at the main window
+    // Render all widgets on the main window
     render_widgets(window);
+    g_print("[+] All widgets have been rendered successfully!\n"); // [DEBUG INFO]
 }
